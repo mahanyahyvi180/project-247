8ab59f2fd9553cc515b5a8ff0d0ed9123d983e37
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-29 21:27:10 +0330

    Add files via upload

---
+++ WindowsFormsApp119/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp119
+{
+    class cMath
+    {
+
+        public static double Factorial(int n)
+        {
+            double s = 1;
+
+            for (int i = n; i >= 1; i--)
+                s *= 1;
+            return s;
+        }
+    }
+}
---
+++ WindowsFormsApp119/Class3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp119
+{
+    class cTarkib
+    {
+
+        public int n
+        {
+            get;
+            set;
+        }
+
+        public int x
+        {
+            get;
+            set;
+        }
+
+        public int combinition(delFac f)
+        {
+            return (int)(f(n) / (f(x) * f(n - x)));
+        }
+    }
+}
